ac242705abbc459bb01a873133ea63fbf353b303
Domi04151309 <domi04151309business@gmail.com>
2019-10-23 12:40:40 +0200

    Tidy up

--- app/src/main/java/com/swdec/paxan/ui/dictionary/DictionaryFragment.kt
+++ app/src/main/java/com/swdec/paxan/ui/dictionary/DictionaryFragment.kt
@@ -7,20 +7,15 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
 import com.swdec.paxan.R
 
 class DictionaryFragment : Fragment() {
-
-    private lateinit var dictionaryViewModel: DictionaryViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dictionaryViewModel =
-            ViewModelProviders.of(this).get(DictionaryViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_dictionary, container, false)
         val listView: ListView = root.findViewById(R.id.listView)
         val values = resources.getStringArray(R.array.dictionary_items)
--- app/src/main/java/com/swdec/paxan/ui/plan/PlanFragment.kt
+++ app/src/main/java/com/swdec/paxan/ui/plan/PlanFragment.kt
@@ -6,23 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ListView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
 import com.swdec.paxan.R
 import android.widget.ArrayAdapter
 
-
-
 class PlanFragment : Fragment() {
-
-    private lateinit var planViewModel: PlanViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        planViewModel =
-            ViewModelProviders.of(this).get(PlanViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_plan, container, false)
         val listView: ListView = root.findViewById(R.id.listView)
         val values = resources.getStringArray(R.array.plan_items)
