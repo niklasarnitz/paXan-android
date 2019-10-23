54a819eedc6ecf443859cd4eaaa45b876d485cae
Domi04151309 <domi04151309business@gmail.com>
2019-10-23 12:12:17 +0200

    Add plan content

--- app/src/main/java/com/swdec/paxan/ui/plan/PlanFragment.kt
+++ app/src/main/java/com/swdec/paxan/ui/plan/PlanFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ListView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.swdec.paxan.R
+import android.widget.ArrayAdapter
+
+
 
 class PlanFragment : Fragment() {
 
@@ -22,10 +24,15 @@ class PlanFragment : Fragment() {
         planViewModel =
             ViewModelProviders.of(this).get(PlanViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_plan, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        planViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+        val listView: ListView = root.findViewById(R.id.listView)
+        val values = resources.getStringArray(R.array.plan_items)
+        val adapter = ArrayAdapter<String>(
+            context!!,
+            android.R.layout.simple_list_item_1,
+            android.R.id.text1,
+            values
+        )
+        listView.adapter = adapter
         return root
     }
 }
