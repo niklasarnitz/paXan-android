0f0650cd575a2c8c4e01e4ca918b11c46dfa78fe
Domi04151309 <domi04151309business@gmail.com>
2019-10-23 12:37:24 +0200

    Prepare map

--- app/src/main/java/com/swdec/paxan/ui/map/MapFragment.kt
+++ app/src/main/java/com/swdec/paxan/ui/map/MapFragment.kt
@@ -5,20 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
 import com.swdec.paxan.R
 
 class MapFragment : Fragment() {
-
-    private lateinit var mapViewModel: MapViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        mapViewModel =
-            ViewModelProviders.of(this).get(MapViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_map, container, false)
         return root
     }
