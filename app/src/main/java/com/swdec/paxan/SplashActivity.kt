54f6fec57884eb756cea4a037ede5e2d2efbf624
Domi04151309 <domi04151309business@gmail.com>
2019-10-30 11:14:57 +0100

    Add splash screen

---
+++ app/src/main/java/com/swdec/paxan/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.swdec.paxan
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.preference.PreferenceManager
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (PreferenceManager.getDefaultSharedPreferences(this).getBoolean("setup_complete", false))
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        else
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        finish()
+    }
+}
