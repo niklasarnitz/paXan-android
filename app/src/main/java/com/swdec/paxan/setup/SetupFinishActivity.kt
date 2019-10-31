a866b0a8049b495beb2dd4a07aee7d850bedb3db
Domi04151309 <domi04151309business@gmail.com>
2019-10-31 17:23:53 +0100

    Save name

--- app/src/main/java/com/swdec/paxan/setup/SetupFinishActivity.kt
+++ app/src/main/java/com/swdec/paxan/setup/SetupFinishActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.preference.PreferenceManager
 import com.swdec.paxan.MainActivity
 import com.swdec.paxan.R
 
@@ -14,7 +15,9 @@ class SetupFinishActivity : AppCompatActivity() {
         setContentView(R.layout.activity_setup_finish)
 
         findViewById<Button>(R.id.continueBtn).setOnClickListener {
+            PreferenceManager.getDefaultSharedPreferences(this).edit().putBoolean("setup_complete", true).apply()
             startActivity(Intent(this, MainActivity::class.java))
+            finish()
         }
     }
 }
--- app/src/main/java/com/swdec/paxan/setup/SetupNameActivity.kt
+++ app/src/main/java/com/swdec/paxan/setup/SetupNameActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import androidx.preference.PreferenceManager
 import com.swdec.paxan.R
 
 class SetupNameActivity : AppCompatActivity() {
@@ -13,6 +15,7 @@ class SetupNameActivity : AppCompatActivity() {
         setContentView(R.layout.activity_setup_name)
 
         findViewById<Button>(R.id.continueBtn).setOnClickListener {
+            PreferenceManager.getDefaultSharedPreferences(this).edit().putString("name", findViewById<EditText>(R.id.nameTxt).text.toString()).apply()
             startActivity(Intent(this, SetupPlanActivity::class.java))
         }
     }
