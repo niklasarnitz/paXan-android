ce0e32c2f69995d8364558724da4e62c4b915286
Domi04151309 <domi04151309business@gmail.com>
2019-11-02 20:33:43 +0100

    Update bottom nav icons

--- app/src/main/java/com/swdec/paxan/main/InfoFragment.kt
+++ app/src/main/java/com/swdec/paxan/main/InfoFragment.kt
@@ -4,10 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ListView
 import androidx.fragment.app.Fragment
 import com.swdec.paxan.R
-import android.widget.ArrayAdapter
 
 class InfoFragment : Fragment() {
 
