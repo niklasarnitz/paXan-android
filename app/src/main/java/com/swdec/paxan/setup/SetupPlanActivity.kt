506ab8c8039bb1f678d0c0defe6cd0799f7e4fd8
Domi04151309 <domi04151309business@gmail.com>
2019-11-09 14:22:23 +0100

    Save plan

--- app/src/main/java/com/swdec/paxan/setup/SetupPlanActivity.kt
+++ app/src/main/java/com/swdec/paxan/setup/SetupPlanActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Spinner
+import androidx.preference.PreferenceManager
 import com.swdec.paxan.R
 
 class SetupPlanActivity : AppCompatActivity() {
@@ -13,6 +15,10 @@ class SetupPlanActivity : AppCompatActivity() {
         setContentView(R.layout.activity_setup_plan)
 
         findViewById<Button>(R.id.continueBtn).setOnClickListener {
+            val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+            prefs.edit().putString("plan_spinner_1", findViewById<Spinner>(R.id.plan_1).selectedItem.toString()).apply()
+            prefs.edit().putString("plan_spinner_2", findViewById<Spinner>(R.id.plan_2).selectedItem.toString()).apply()
+            prefs.edit().putString("plan_spinner_3", findViewById<Spinner>(R.id.plan_3).selectedItem.toString()).apply()
             startActivity(Intent(this, SetupFinishActivity::class.java))
         }
     }
