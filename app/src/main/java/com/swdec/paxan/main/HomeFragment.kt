1ba4d50692c6759e3778eb2e495a54dcce4ab9ee
Domi04151309 <domi04151309business@gmail.com>
2019-11-10 14:40:16 +0100

    Add name changing

--- app/src/main/java/com/swdec/paxan/main/HomeFragment.kt
+++ app/src/main/java/com/swdec/paxan/main/HomeFragment.kt
@@ -1,15 +1,20 @@
 package com.swdec.paxan.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.preference.PreferenceManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.swdec.paxan.R
+import com.swdec.paxan.settings.SettingsNameActivity
 
 class HomeFragment : Fragment() {
+
+    private var titleTxt: TextView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,10 +22,20 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val prefs = PreferenceManager.getDefaultSharedPreferences(context!!)
-        var name = prefs.getString("name", "Gast") ?:"Gast"
-        if (name == "") name = "Gast"
-        root.findViewById<TextView>(R.id.titleTxt).text = resources.getString(R.string.home_title, name)
+        titleTxt = root.findViewById(R.id.titleTxt)
+        root.findViewById<Button>(R.id.nameBtn).setOnClickListener {
+            startActivity(Intent(context, SettingsNameActivity::class.java))
+        }
         return root
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val prefs = PreferenceManager.getDefaultSharedPreferences(context!!)
+        val defaultName = resources.getString(R.string.default_name)
+        var name = prefs.getString("name", defaultName) ?:defaultName
+        if (name == "") name = defaultName
+        titleTxt!!.text = resources.getString(R.string.home_title, name)
     }
 }
