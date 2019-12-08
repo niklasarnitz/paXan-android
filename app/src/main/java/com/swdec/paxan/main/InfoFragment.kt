eb1758bfc1b0f0f1de3e1a173fe0dd7f5acb1349
Domi04151309 <domi04151309business@gmail.com>
2019-12-08 17:51:13 +0100

    Add activities for info buttons

--- app/src/main/java/com/swdec/paxan/main/InfoFragment.kt
+++ app/src/main/java/com/swdec/paxan/main/InfoFragment.kt
@@ -1,11 +1,13 @@
 package com.swdec.paxan.main
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import com.swdec.paxan.R
+import com.swdec.paxan.*
 
 class InfoFragment : Fragment() {
 
@@ -15,6 +17,21 @@ class InfoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_main_info, container, false)
+        root.findViewById<Button>(R.id.speakers).setOnClickListener {
+            startActivity(Intent(context, SpeakersActivity::class.java))
+        }
+        root.findViewById<Button>(R.id.seminars).setOnClickListener {
+            startActivity(Intent(context, SeminarsActivity::class.java))
+        }
+        root.findViewById<Button>(R.id.donate).setOnClickListener {
+            startActivity(Intent(context, DonateActivity::class.java))
+        }
+        root.findViewById<Button>(R.id.timeline).setOnClickListener {
+            startActivity(Intent(context, TimelineActivity::class.java))
+        }
+        root.findViewById<Button>(R.id.about).setOnClickListener {
+            startActivity(Intent(context, AboutActivity::class.java))
+        }
         return root
     }
 }
