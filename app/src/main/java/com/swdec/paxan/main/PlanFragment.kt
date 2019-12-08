5fba9db20676054d3b1422f15c4a1c0c861b43c7
Domi04151309 <domi04151309business@gmail.com>
2019-12-08 18:13:49 +0100

    Add SeminarDetailActivity

--- app/src/main/java/com/swdec/paxan/main/PlanFragment.kt
+++ app/src/main/java/com/swdec/paxan/main/PlanFragment.kt
@@ -6,10 +6,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import com.swdec.paxan.R
 import android.widget.TextView
 import androidx.preference.PreferenceManager
+import com.swdec.paxan.SeminarDetailActivity
 import com.swdec.paxan.settings.SettingsPlanActivity
 
 class PlanFragment : Fragment() {
@@ -27,6 +29,15 @@ class PlanFragment : Fragment() {
         plan1 = root.findViewById(R.id.plan_1_txt)
         plan2 = root.findViewById(R.id.plan_2_txt)
         plan3 = root.findViewById(R.id.plan_3_txt)
+        root.findViewById<LinearLayout>(R.id.plan_1_click).setOnClickListener {
+            startActivity(Intent(context, SeminarDetailActivity::class.java))
+        }
+        root.findViewById<LinearLayout>(R.id.plan_2_click).setOnClickListener {
+            startActivity(Intent(context, SeminarDetailActivity::class.java))
+        }
+        root.findViewById<LinearLayout>(R.id.plan_3_click).setOnClickListener {
+            startActivity(Intent(context, SeminarDetailActivity::class.java))
+        }
         root.findViewById<Button>(R.id.planBtn).setOnClickListener {
             startActivity(Intent(context, SettingsPlanActivity::class.java))
         }
