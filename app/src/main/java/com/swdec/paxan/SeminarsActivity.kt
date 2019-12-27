3ff832aaab9a2f75e17691448657037929d436f7
Domi04151309 <domi04151309business@gmail.com>
2019-12-27 17:51:51 +0100

    Add and improve lists

--- app/src/main/java/com/swdec/paxan/SeminarsActivity.kt
+++ app/src/main/java/com/swdec/paxan/SeminarsActivity.kt
@@ -1,12 +1,29 @@
 package com.swdec.paxan
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class SeminarsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_seminars)
+
+        val listView: ListView = findViewById(R.id.listView)
+        val values = resources.getStringArray(R.array.dummy_array)
+        val adapter = ArrayAdapter<String>(
+            this,
+            R.layout.list_item_seminar,
+            R.id.text1,
+            values
+        )
+        listView.adapter = adapter
+        listView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            startActivity(Intent(this, SeminarDetailActivity::class.java).putExtra("position", position))
+        }
     }
 }
--- app/src/main/java/com/swdec/paxan/SpeakersActivity.kt
+++ app/src/main/java/com/swdec/paxan/SpeakersActivity.kt
@@ -2,11 +2,27 @@ package com.swdec.paxan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class SpeakersActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_speakers)
+
+        val listView: ListView = findViewById(R.id.listView)
+        val values = resources.getStringArray(R.array.dummy_array)
+        val adapter = ArrayAdapter<String>(
+            this,
+            R.layout.list_item_speakers,
+            R.id.text1,
+            values
+        )
+        listView.adapter = adapter
+        listView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            //startActivity(Intent(this, SpeakerDetailActivity::class.java).putExtra("position", position))
+        }
     }
 }
