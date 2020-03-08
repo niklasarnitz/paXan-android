75fc2627d258880f764877ce953f34f37087c04d
Domi04151309 <domi04151309business@gmail.com>
2020-03-08 17:59:34 +0100

    Remove google map

--- app/src/main/java/com/swdec/paxan/main/MapFragment.kt
+++ app/src/main/java/com/swdec/paxan/main/MapFragment.kt
@@ -4,19 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.swdec.paxan.R
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.model.LatLng
-import android.content.pm.PackageManager
-import androidx.core.app.ActivityCompat
-import androidx.core.app.ActivityCompat.requestPermissions
 
 
 class MapFragment : Fragment() {
-    private var mapView: MapView? = null
+    private var mapView: TextView? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,52 +19,6 @@ class MapFragment : Fragment() {
     ): View? {
         val root = inflater.inflate(R.layout.fragment_main_map, container, false)
         mapView = root.findViewById(R.id.mapView)
-        mapView!!.onCreate(savedInstanceState)
-        mapView!!.getMapAsync(this::setUpMaps)
         return root
-    }
-
-    private fun setUpMaps(googleMap: GoogleMap) {
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(48.796355, 8.503494), 18.5F))
-        googleMap.uiSettings.isMyLocationButtonEnabled = false
-        if (ActivityCompat.checkSelfPermission(
-                context!!,
-                android.Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                context!!,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            requestPermissions(
-                activity!!,
-                arrayOf(
-                    android.Manifest.permission.ACCESS_COARSE_LOCATION,
-                    android.Manifest.permission.ACCESS_FINE_LOCATION
-                ),
-                1
-            )
-            return
-        }
-        googleMap.isMyLocationEnabled = true
-    }
-
-    override fun onResume() {
-        mapView!!.onResume()
-        super.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        mapView!!.onPause()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mapView!!.onDestroy()
-    }
-
-    override fun onLowMemory() {
-        super.onLowMemory()
-        mapView!!.onLowMemory()
     }
 }
